76418a5d85669a891285af1806d1b64a7090bfb7
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-10-06 20:14:38 +0900

    loops-and-functions

---
+++ loops-and-functions.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "fmt"
+  "math"
+)
+
+func Sqrt(x float64) float64 {
+  var numOfLoops int = 0
+  var z float64 = 1.0
+  for {
+    nextZ := z - (z * z - x) / (2 * z)
+    if math.Abs(nextZ - z) < 0.000000000000001 {
+      break
+    }
+    z = nextZ
+    numOfLoops++
+  }
+  fmt.Println("Number of loops: " + fmt.Sprint(numOfLoops))
+  return z
+}
+
+func main() {
+  fmt.Println(Sqrt(2))
+}
