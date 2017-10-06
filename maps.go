f7ab7ce1696b61587161784ca47c024d41167415
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-10-06 20:15:31 +0900

    maps

---
+++ maps.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	var wordCount = make(map[string]int)
+	for _, word := range strings.Fields(s) {
+		wordCount[word]++
+	}
+	return wordCount
+}
+
+func main() {
+	wc.Test(WordCount)
+}
