dc74e4b78aa5bae0c7bc826c2da0468140ce9650
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-10-06 20:15:40 +0900

    fibonacci-closure

---
+++ fibonacci-closure.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+  var secondLast int = 0
+	var last int = 1
+	return func() int {
+		temp := secondLast
+		next := last + secondLast
+		secondLast = last
+		last = next
+		return temp
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
