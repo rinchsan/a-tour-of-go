7d2e599830b05feae6900e0dd077dd1fd74a5fa1
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-10-06 20:15:25 +0900

    slices

---
+++ slices.go
@@ -0,0 +1,20 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	var pic [][]uint8
+	for y := 0; y < dy; y++ {
+		var line []uint8
+		for x := 0; x < dx; x++ {
+			val := x^y
+			line = append(line, uint8(val))
+		}
+		pic = append(pic, line)
+	}
+	return pic
+}
+
+func main() {
+	pic.Show(Pic)
+}
